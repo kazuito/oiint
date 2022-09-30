719df91baa079be87c5f64c245bd7be4797ac90e
Zarigani <zari220130@gmail.com>
2022-09-30 21:17:18 +0900

    Update

--- content-script.js
+++ content-script.js
@@ -1,16 +1,22 @@
+var url = "";
+
 $(document).on(
   "click",
   "a[data-nav='1'][tabindex='0'][data-navigation='server']",
   function (e) {
-    let href = e.currentTarget.href;
-    let decoded = decodeURIComponent(href);
-    let m = decoded.match(/(?<=imgurl=).*?(?=\&imgrefurl=)/);
-    replaceSrc(m[0]);
+    console.log(e);
+    const href = decodeURIComponent(e.currentTarget.href);
+    url = href.match(/(?<=imgurl=).*?(?=\&imgrefurl=)/)[0];
   }
 );
 
-function replaceSrc(src) {
-  $(
-    "div > a[role='link'][tabindex='0'][rel='noopener'][target='_blank']:has(img)"
-  ).attr("href", src);
+$(document).on("mouseup", "a", function (e) {
+  setHref(url);
+});
+
+function setHref(url) {
+  const a = $(
+    "div > div > c-wiz[data-hveid='0'][decode-data-ved='1'][data-a='1'] > div > div > div > div > div[role='region'] > a[role='link'][tabindex='0'][rel='noopener'][target='_blank']"
+  );
+  a.attr("href", url);
 }
