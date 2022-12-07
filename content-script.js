6dc29ed2d1516bdb805ca2609731925d67b6780c
Zarigani <zari220130@gmail.com>
2022-12-07 10:38:08 +0900

    Fix bug

--- content-script.js
+++ content-script.js
@@ -1,22 +1,26 @@
-var url = "";
+let url = "";
 
+// Get url by click
 $(document).on(
   "click",
   "a[data-nav='1'][tabindex='0'][data-navigation='server']",
   function (e) {
-    console.log(e);
     const href = decodeURIComponent(e.currentTarget.href);
     url = href.match(/(?<=imgurl=).*?(?=\&imgrefurl=)/)[0];
   }
 );
 
+// Set new href
+function setHref(url) {
+  const targets = $(
+    `c-wiz > div > div > div > div > div > a[role="link"][tabindex="0"][rel="noopener"][target="_blank"]`
+  );
+
+  targets.each(function () {
+    $(this).attr("href", url);
+  });
+}
+
 $(document).on("mouseup", "a", function (e) {
   setHref(url);
 });
-
-function setHref(url) {
-  const a = $(
-    "div > div > c-wiz[data-hveid='0'][decode-data-ved='1'][data-a='1'] > div > div > div > div > div[role='region'] > a[role='link'][tabindex='0'][rel='noopener'][target='_blank']"
-  );
-  a.attr("href", url);
-}
